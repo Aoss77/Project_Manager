3431ff81b42f708f38b29c556ced31e3377e0782
Aoss77 <Essam.El.Arabi@gmail.com>
2023-03-31 17:20:41 +0400

    add authorization

--- Projects/views.py
+++ Projects/views.py
@@ -4,7 +4,7 @@ from Projects.forms import ProjectCreateForm, ProjectUpdateForm
 from django.views.generic import ListView, DeleteView, CreateView, UpdateView
 from django.urls import reverse, reverse_lazy
 from . import models, forms
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 # Create your views here.
@@ -15,57 +15,77 @@ class ProjectListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         query_set = super().get_queryset()
-        where = {}
+        where = {'user_id': self.request.user}
         q = self.request.GET.get('q', None)
         if q: where['title__icontains'] = q
         return query_set.filter(**where)
 
 
-class ProjectCreateView(LoginRequiredMixin,CreateView):
+class ProjectCreateView(LoginRequiredMixin, CreateView):
     model = models.Projects
     form_class = forms.ProjectCreateForm
     template_name = "project/create.html"
     success_url = reverse_lazy('project_list')
 
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
-class ProjectUpdateView(LoginRequiredMixin,UpdateView):
+
+class ProjectUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = models.Projects
     form_class = forms.ProjectUpdateForm
     template_name = "project/update.html"
     success_url = reverse_lazy('project_list')
     context_object_name = "project"
 
+    def test_func(self):
+        return self.get_object().user_id == self.request.user.id
+
     def get_success_url(self):
         return reverse('project_update', args=[self.object.id])
 
 
-class ProjectDeleteView(LoginRequiredMixin,DeleteView):
+class ProjectDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = models.Projects
     template_name = "project/delete.html"
     success_url = reverse_lazy('project_list')
     context_object_name = "project"
 
+    def test_func(self):
+        return self.get_object().user_id == self.request.user.id
 
-class TaskCreateView(LoginRequiredMixin,CreateView):
+
+class TaskCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = models.Task
     fields = ['project', 'description']
     http_method_names = ['post']
 
+    def test_func(self):
+        project_id = self.request.POST.get('project', '')
+        return models.Projects.objects.get(pk=project_id).user_id == self.request.user.id
+
     def get_success_url(self):
         return reverse('project_update', args=[self.object.project.id])
 
 
-class TaskUpdateView(LoginRequiredMixin,UpdateView):
+class TaskUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = models.Task
     fields = ['is_completed']
     http_method_names = ['post']
 
+
+    def test_func(self):
+        return self.get_object().project.user_id == self.request.user.id
+
     def get_success_url(self):
         return reverse('project_update', args=[self.object.project.id])
 
 
-class TaskDeleteView(LoginRequiredMixin,DeleteView):
+class TaskDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = models.Task
 
+    def test_func(self):
+        return self.get_object().project.user_id == self.request.user.id
     def get_success_url(self):
         return reverse('project_update', args=[self.object.project.id])
