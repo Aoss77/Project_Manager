eb5f3620aeb63b5be3832f3a8acd94402aca4671
Aoss77 <Essam.El.Arabi@gmail.com>
2023-03-31 15:18:23 +0400

    add new account

--- Accounts/forms.py
+++ Accounts/forms.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
 from django import forms
 
 field_attrs = {'class': 'form-control'}
@@ -13,7 +14,52 @@ class UserLoginForm(AuthenticationForm):
         label='Username',
         widget=forms.TextInput(attrs=field_attrs),
     )
-    password= forms.CharField(
+    password = forms.CharField(
         label='Password',
         widget=forms.TextInput(attrs=field_attrs),
     )
+
+
+class UserCreateForm(UserCreationForm):
+    first_name = forms.CharField(
+        label='First Name',
+        widget=forms.TextInput(attrs=field_attrs),
+    )
+    last_name = forms.CharField(
+        label='Last name',
+        widget=forms.TextInput(attrs=field_attrs),
+    )
+    username = forms.CharField(
+        label='Username',
+        widget=forms.TextInput(attrs=field_attrs),
+    )
+    email = forms.EmailField(
+        label='email',
+        widget=forms.TextInput(attrs=field_attrs),
+    )
+    password1 = forms.CharField(
+        label='Password',
+        strip=False,
+        widget=forms.TextInput(attrs=field_attrs),
+    )
+    password2 = forms.CharField(
+        label='Password Confirmation',
+        strip=False,
+        widget=forms.TextInput(attrs=field_attrs),
+    )
+
+    class Meta(UserCreationForm.Meta):
+        fields = ('first_name', 'last_name', 'username', 'email')
+
+
+class ProfileForm(UserChangeForm):
+    password = None
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+        widgets = {
+            'first_name': forms.TextInput(attrs=field_attrs),
+            'last_name': forms.TextInput(attrs=field_attrs),
+            'email': forms.EmailInput(attrs=field_attrs),
+        }
